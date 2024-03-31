1daebd54a5d44cb21c02d24244b45ba39e62ab37
tingwry <weeraya972546@gmail.com>
2024-03-31 12:57:16 +0700

    feat: countdown timer frontend

--- frontend/app/src/CountDownTimer.tsx
+++ frontend/app/src/CountDownTimer.tsx
@@ -8,9 +8,10 @@ import {
   Pressable,
   Button,
   Image,
+  Dimensions,
 } from "react-native";
-import SvgUri from "react-native-svg";
 import { theme } from "../theme";
+import { LinearGradient } from "expo-linear-gradient";
 
 const CountdownTimer = () => {
   const [fontsLoaded] = useFonts({
@@ -24,6 +25,7 @@ const CountdownTimer = () => {
   }
   const [time, setTime] = useState(25 * 60); // 25 minutes in seconds
   const [modalVisible, setModalVisible] = useState(false);
+  const [remainingTime, setRemainingTime] = useState<number | null>(null);
 
   useEffect(() => {
     let interval: NodeJS.Timeout | null = null;
@@ -31,6 +33,7 @@ const CountdownTimer = () => {
     if (time > 0) {
       interval = setInterval(() => {
         setTime((prevTime) => prevTime - 1);
+        setRemainingTime(time);
       }, 1000);
     }
     return () => {
@@ -47,10 +50,15 @@ const CountdownTimer = () => {
       .padStart(2, "0")}`;
   };
 
+  const stopCountdown = () => {
+    setModalVisible(false);
+    console.log(time);
+    console.log(remainingTime);
+    setTime(0);
+  };
+
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.timer}>{formatTime(time)}</Text>
-       */}
       <Pressable onPress={() => setModalVisible(true)}>
         <Text>Show Timer</Text>
       </Pressable>
@@ -68,17 +76,26 @@ const CountdownTimer = () => {
             >
               <Image
                 style={{ width: 32, resizeMode: "contain" }}
-                source={require("../../assets/images/button-close.png")}
+                source={require("../../assets/icons/button-close.png")}
               />
             </Pressable>
+
             <Text style={styles.modalHeader}>Departure time begins in</Text>
+
             <Text style={styles.timer}>{formatTime(time)}</Text>
+
             <Pressable
-              style={styles.button}
-              onPress={() => setModalVisible(false)}
+              onPress={stopCountdown}
+              style={[styles.buttonContainer, styles.shadowProp]}
             >
-              <Text style={styles.text}>Stop Countdown</Text>
+              <LinearGradient
+                colors={["#CF7B04", "#EDA33C"]}
+                style={styles.buttonStyle}
+              >
+                <Text style={styles.buttonText}>Stop Countdown</Text>
+              </LinearGradient>
             </Pressable>
+
             <View
               style={{
                 flexDirection: "row",
@@ -86,8 +103,13 @@ const CountdownTimer = () => {
               }}
             >
               <Image
-                style={{ marginTop: 20, marginRight: 5 }}
-                source={require("../../assets/images/alert-circle.png")}
+                style={{
+                  marginTop: 5,
+                  marginRight: 5,
+                  width: 16,
+                  resizeMode: "contain",
+                }}
+                source={require("../../assets/icons/alert-circle.png")}
               />
               <Text style={styles.modalText}>
                 Press this button after leaving your home to stop the countdown.
@@ -100,6 +122,8 @@ const CountdownTimer = () => {
     </View>
   );
 };
+
+const screenWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   container: {
@@ -145,25 +169,31 @@ const styles = StyleSheet.create({
   },
   closeButton: {
     position: "absolute",
-    top: 10,
+    top: -20,
     right: 10,
   },
-  button: {
+  buttonContainer: {
     marginTop: 20,
+    width: "100%",
     alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 12,
-    paddingHorizontal: 70,
-    borderRadius: 18,
-    elevation: 3,
-    backgroundColor: "rgba(207, 123, 4, 1)",
   },
-  text: {
+  buttonStyle: {
+    width: screenWidth - 112, // Adjusting width considering left and right margin of 32
+    height: 48,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontFamily: "dm-sans-extrabold",
+    color: theme.colors.textPrimary,
     fontSize: 16,
-    lineHeight: 21,
-    fontWeight: "bold",
-    letterSpacing: 0.25,
-    color: "white",
+  },
+  shadowProp: {
+    shadowColor: theme.colors.textPrimary,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
   },
 });
 
