a60e90d8ee82719851a68ba22892ab8ca63600c7
fahsanu <88034258+fahsanu@users.noreply.github.com>
2024-04-09 02:14:05 +0700

    fix: migrate db again, add location

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserInfo, Schedule
+from .models import UserInfo, Schedule, Location
 
 # Register your models here.
 admin.site.register(UserInfo)
 admin.site.register(Schedule)
+admin.site.register(Location)
