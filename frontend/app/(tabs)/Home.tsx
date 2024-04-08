9a1a9578e58ed7f886033e3c86eab5afa2d9756b
fahsanu <88034258+fahsanu@users.noreply.github.com>
2024-04-08 22:48:27 +0700

    feat: add MapView package

--- frontend/app/(tabs)/Home.tsx
+++ frontend/app/(tabs)/Home.tsx
@@ -1,3 +1,61 @@
+// import React, { useRef, useState } from "react";
+// import { Button, StyleSheet, Text, View } from "react-native";
+// import MapView from "react-native-maps";
+// import { Marker } from "react-native-maps";
+
+// export default function App() {
+//   const mapRef = useRef(null);
+//   const [region, setRegion] = useState({
+//     latitude: 51.5079145,
+//     longitude: -0.0899163,
+//     latitudeDelta: 0.01,
+//     longitudeDelta: 0.01,
+//   });
+//   const tokyoRegion = {
+//     latitude: 35.6762,
+//     longitude: 139.6503,
+//     latitudeDelta: 0.01,
+//     longitudeDelta: 0.01,
+//   };
+//   const goToTokyo = () => {
+//     //complete this animation in 3 seconds
+//     mapRef.current.animateToRegion(tokyoRegion, 3 * 1000);
+//   };
+//   return (
+//     <View style={styles.container}>
+//       <MapView
+//         ref={mapRef}
+//         style={styles.map}
+//         initialRegion={{
+//           latitude: 24.8607,
+//           longitude: 67.0011,
+//           latitudeDelta: 0.0922,
+//           longitudeDelta: 0.0421,
+//         }}
+//         onRegionChangeComplete={(region) => setRegion(region)}
+//       />
+//       <Button onPress={() => goToTokyo()} title="Go to Tokyo" />
+//       <Text style={styles.text}>Current latitude{region.latitude}</Text>
+//       <Text style={styles.text}>Current longitude{region.longitude}</Text>
+//     </View>
+//   );
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     ...StyleSheet.absoluteFillObject,
+//     flex: 1,
+//     justifyContent: "flex-end",
+//     alignItems: "center",
+//   },
+//   map: {
+//     ...StyleSheet.absoluteFillObject,
+//   },
+//   text: {
+//     fontSize: 20,
+//     backgroundColor: "lightblue",
+//   },
+// });
+
 import { View, Text, StyleSheet, Image, StatusBar, Button } from "react-native";
 import React, { useState, useEffect } from "react";
 import { theme } from "../theme";
