2311b1d3172db09199cd5514b3438a26412ecb08
fahsanu <88034258+fahsanu@users.noreply.github.com>
2024-04-15 09:51:37 +1000

    fix: more location able to add new

--- frontend/app/(locations)/MapLocation.tsx
+++ frontend/app/(locations)/MapLocation.tsx
@@ -5,52 +5,52 @@ import MapView from "react-native-maps";
 import { Marker, Callout } from "react-native-maps";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
-
 export default function MapLocation() {
-  const [pin, setPin] = useState({ 
+  const [pin, setPin] = useState({
     latitude: 13.736834400006273,
-    longitude: 100.53314465311604, 
-});
-  const [region, setRegion] = useState({ 
-      latitude: 13.736834400006273,
-      longitude: 100.53314465311604, });
+    longitude: 100.53314465311604,
+  });
+
   const mapRef = useRef(null);
-  
-//   const goToPin = () => {
-//     //complete this animation in 3 seconds
-//     mapRef.current.animateToRegion(pin, 3 * 1000);
-//   };
 
   const onMarkerDragEnd = (e: any) => {
     const { latitude, longitude } = e.nativeEvent.coordinate;
 
-    const validLatitude = Math.min(Math.max(latitude, -90), 90).toFixed(16);
-    const validLongitude = Math.min(Math.max(longitude, -180), 180).toFixed(16);
+    const validLatitude = parseFloat(
+      Math.min(Math.max(latitude, -90), 90).toFixed(16)
+    );
+    const validLongitude = parseFloat(
+      Math.min(Math.max(longitude, -180), 180).toFixed(16)
+    );
 
-    setPin(prevPin => ({ ...prevPin, validLatitude, validLongitude }));
-    console.log("End", pin);
+    setPin({ latitude: validLatitude, longitude: validLongitude });
+    console.log("End", { latitude: validLatitude, longitude: validLongitude });
   };
 
   const handleSubmit = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/app/location/create/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          loc_name: "Home",
-          latitude: pin.latitude,
-          longitude: pin.longitude,
-          default_home: false,
-          default_dest: false,
-          user_id: 1,
-        }),
-      });
+      const response = await fetch(
+        "http://127.0.0.1:8000/app/location/create/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            loc_name: "Home",
+            latitude: pin.latitude,
+            longitude: pin.longitude,
+            default_home: false,
+            default_dest: false,
+            uid: 1,
+          }),
+        }
+      );
       if (response.ok) {
         console.log("Success");
       } else {
         console.error("Failed to post");
+        console.log(response.status)
       }
     } catch (error) {
       console.error("Error submitting:", error);
@@ -87,30 +87,23 @@ export default function MapLocation() {
       <MapView
         // provider="google"
         ref={mapRef}
-        style={styles.map}
+        style={{ width: "100%", height: "100%", marginTop: 20 }}
         initialRegion={{
           latitude: 13.736834400006273,
           longitude: 100.53314465311604,
           latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421, 
+          longitudeDelta: 0.0421,
         }}
       >
-        <Marker
-          coordinate={pin}
-          draggable={true}
-          onDragStart={(e) => 
-            console.log("Start", e.nativeEvent.coordinate
-          )}
-          onDragEnd={onMarkerDragEnd} 
-        >
+        <Button onPress={handleSubmit} title="Submit" />
+        <Marker coordinate={pin} draggable={true} onDragEnd={onMarkerDragEnd}>
           <Callout>
-            <Text>My Location</Text>
+            <Text>My Destination</Text>
           </Callout>
         </Marker>
       </MapView>
-      <Button onPress={handleSubmit} title="Submit" />
+      
       {/* <Button onPress={() => goToPin()} title="Go Home" /> */}
-
     </View>
   );
 }
