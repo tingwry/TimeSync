7851fa73603c5afdda4af597c9ea26c7992d3153
tingwry <weeraya972546@gmail.com>
2024-04-18 22:07:49 +0700

    feat: view prep time api

--- backend/app/api/preptime/urls.py
+++ backend/app/api/preptime/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create/', views.TotalPrepTimeCreate.as_view()),
+    path('view/', views.TotalPrepTimeViewAll.as_view()),
+    path('recent/', views.TotalPrepTimeViewRecent.as_view()),  
 ]
--- backend/app/api/preptime/views.py
+++ backend/app/api/preptime/views.py
@@ -8,3 +8,18 @@ from ...serializers import TotalPrepTimeSerializer
 class TotalPrepTimeCreate(generics.CreateAPIView):
     queryset = TotalPrepTime.objects.all()
     serializer_class = TotalPrepTimeSerializer
+
+# view all prep time records
+class TotalPrepTimeViewAll(generics.ListAPIView):
+    queryset = TotalPrepTime.objects.all()
+    serializer_class = TotalPrepTimeSerializer
+
+# view most recent prep time
+class TotalPrepTimeViewRecent(generics.RetrieveAPIView):
+    serializer_class = TotalPrepTimeSerializer
+
+    def get_object(self):
+        # Retrieve the most recent prep time based on the iteration
+        most_recent_preptime = TotalPrepTime.objects.order_by('-iteration').first()
+        
+        return most_recent_preptime
