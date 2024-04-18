7ae00a9417da9a0c5036195af9c0f44eba5da579
Thenampetch <punpaporn.saa@gmail.com>
2024-04-18 21:29:31 +0700

    GETdone

--- backend/app/api/ML/urls.py
+++ backend/app/api/ML/urls.py
@@ -7,18 +7,3 @@ from .views import (
 urlpatterns = [
     path('predict/', MLView.as_view() , name='predict_times'),
 ]
-
-# from django.urls import path
-# from .views import (MLModel)
-
-# urlpatterns = [
-#     path('train/', MLModel.as_view(), name='train-models'),
-#     path('predict/', MLModel.as_view(), name='predict-times'),
-# ]
-
-# from django.urls import path
-# from .views import (predict_times)
-
-# urlpatterns = [
-#     path('predict/', predict_times , name='predict_times'),
-# ]
